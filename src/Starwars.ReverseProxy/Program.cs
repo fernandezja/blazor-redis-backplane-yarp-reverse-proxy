621d9a4dcb1d578d1aca5c90b9c6c47673a99927
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-06-04 19:50:27 -0300

    ReverseProxy add  AddServiceDefaults

--- src/Starwars.ReverseProxy/Program.cs
+++ src/Starwars.ReverseProxy/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.AddServiceDefaults();
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
